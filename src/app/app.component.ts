4b96a56daa634bb40d7980dc314344711eec93d6
mendozanmartin <mendozanmartin@gmail.com>
2019-11-01 15:10:28 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'personal-website';
+
+  downloadResume() {
+    window.open('../assets/assets/Resume.pdf', '_blank')
+  }
+
+  openWebsite(website: string) {
+    switch (website) {
+      case "linkedIn":
+          window.open(
+              'https://www.linkedin.com/in/martin-mendoza/',
+              '_blank' // <- This is what makes it open in a new window.
+          );
+          break;
+      case "github":
+          window.open(
+              'https://github.com/mendozanmartin',
+              '_blank'
+          );
+          break;
+      case "email":
+          window.open(
+              'mailto:mendozan@mcmaster.ca',
+              '_blank'
+          );
+          break;
+      case "vimeo":
+          window.open(
+              'https://vimeo.com/user100529261',
+              '_blank'
+          );
+  }
+  }
 }
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDBLcBEQjB8KM333mD7QL2TrRQXzEUtYfQ",
+    authDomain: "personal-website-ccd53.firebaseapp.com",
+    databaseURL: "https://personal-website-ccd53.firebaseio.com",
+    projectId: "personal-website-ccd53",
+    storageBucket: "personal-website-ccd53.appspot.com",
+    messagingSenderId: "878420998734",
+    appId: "1:878420998734:web:ac39757289101dc7efe11f",
+    measurementId: "G-1GPZPG7KFD"
+  }
 };
 
 /*
