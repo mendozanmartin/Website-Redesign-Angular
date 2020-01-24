173aa22ba4bbb2ec740cc39db8de61e5d244a51f
Martin <mendozanmartin@gmail.com>
2020-01-24 00:05:42 -0500

    fix small bugs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,7 +15,7 @@ export class HomeComponent implements OnInit {
   }
 
   private loadScripts() {
-    this.dynamicScriptLoader.load('vendorjs', 'appjs').then(data => {
+    this.dynamicScriptLoader.load('vendorjs').then(data => {
       console.log(data)
       console.log("Scripts loaded succesfully")
     }).catch(error => console.log(error))
