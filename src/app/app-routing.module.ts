0fb593b0ea25b5e169c47526e420b44eab6a5e32
Martin <mendozanmartin@gmail.com>
2020-01-30 11:24:31 -0500

    more content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -62,8 +62,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-  })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
