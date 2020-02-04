57d97edfd78e81d807164ee8a9ac025cd59151bf
Martin <mendozanmartin@gmail.com>
2020-02-04 14:51:31 -0500

    more content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AngularFireModule } from '@angular/fire';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { environment } from '../environments/environment';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { OrdrupComponent } from './ordrup/ordrup.component';
-import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { PromptuComponent } from './promptu/promptu.component';
-import { ChallengeforchangeComponent } from './challengeforchange/challengeforchange.component';
-import { RefugeevrexperienceComponent } from './refugeevrexperience/refugeevrexperience.component';
-import { JackstonesComponent } from './jackstones/jackstones.component';
-import { ArduinovacuumComponent } from './arduinovacuum/arduinovacuum.component';
-import { RadiothonboothComponent } from './radiothonbooth/radiothonbooth.component';
-import { EcosnapComponent } from './ecosnap/ecosnap.component';
-import { MedexpressComponent } from './medexpress/medexpress.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { environment } from "../environments/environment";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { OrdrupComponent } from "./ordrup/ordrup.component";
+import { HomeComponent } from "./home/home.component";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { PromptuComponent } from "./promptu/promptu.component";
+import { ChallengeforchangeComponent } from "./challengeforchange/challengeforchange.component";
+import { RefugeevrexperienceComponent } from "./refugeevrexperience/refugeevrexperience.component";
+import { JackstonesComponent } from "./jackstones/jackstones.component";
+import { ArduinovacuumComponent } from "./arduinovacuum/arduinovacuum.component";
+import { RadiothonboothComponent } from "./radiothonbooth/radiothonbooth.component";
+import { EcosnapComponent } from "./ecosnap/ecosnap.component";
+import { MedexpressComponent } from "./medexpress/medexpress.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgxPageScrollCoreModule } from "ngx-page-scroll-core";
 
 @NgModule({
   declarations: [
@@ -41,9 +42,10 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgxPageScrollCoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,21 +1,28 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-
+import { Component, OnInit, AfterViewChecked } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"]
 })
-
-
-export class HeaderComponent implements OnInit {
-
-
-  constructor() { }
-
-  ngOnInit() {
+export class HeaderComponent implements OnInit, AfterViewChecked {
+  public fragment: any;
+  constructor(private route: ActivatedRoute) {
+    this.route.fragment.subscribe(fragment => {
+      this.fragment = fragment;
+    });
   }
 
+  ngAfterViewChecked(): void {
+    try {
+      if (this.fragment) {
+        document
+          .querySelector("#" + this.fragment)
+          .scrollIntoView({ behavior: "smooth" });
+      }
+    } catch (e) {}
+  }
 
-
+  ngOnInit() {}
 }
