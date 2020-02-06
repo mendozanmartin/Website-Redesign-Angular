b5586db565a8794fd6b21450ce8560c824f189ab
Martin <mendozanmartin@gmail.com>
2020-02-06 13:39:26 -0500

    change header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,23 +6,6 @@ import { ActivatedRoute } from "@angular/router";
   templateUrl: "./header.component.html",
   styleUrls: ["./header.component.scss"]
 })
-export class HeaderComponent implements OnInit, AfterViewChecked {
-  public fragment: any;
-  constructor(private route: ActivatedRoute) {
-    this.route.fragment.subscribe(fragment => {
-      this.fragment = fragment;
-    });
-  }
-
-  ngAfterViewChecked(): void {
-    try {
-      if (this.fragment) {
-        document
-          .querySelector("#" + this.fragment)
-          .scrollIntoView({ behavior: "smooth" });
-      }
-    } catch (e) {}
-  }
-
+export class HeaderComponent implements OnInit {
   ngOnInit() {}
 }
